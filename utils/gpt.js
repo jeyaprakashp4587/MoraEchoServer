3cd1096fd1009f5899524fb169d8db4f7fea85e3
jeyaprakshp4587 <jeyaprakashp022@gmail.com>
2025-11-21 23:54:40 +0530

    update

--- utils/gpt.js
+++ utils/gpt.js
@@ -1,36 +1,55 @@
-import dotenv from "dotenv";
 import OpenAI from "openai";
 import { fillTemplate } from "./fillTemplate.js";
 import DB1 from "../DB/DB1.js";
+import dotenv from "dotenv";
+import { getCache, setCache } from "../Redis/redis.js";
 dotenv.config();
-const openai = new OpenAI({
-  apiKey: process.env.GPT_API_KEY,
-});
+const openai = new OpenAI({ apiKey: process.env.GPT_API_KEY });
 
-export const getGPTResponse = async (chatType = "Mora", person, newMessage) => {
+export const getGPTResponse = async (
+  chatType = "Mora",
+  person,
+  newMessage,
+  historyMessages = []
+) => {
   try {
-    // Fetch model by name
+    // Fetch model data
+    const cachedModelData = await getCache("modelData");
+    let modelData = cachedModelData;
+    // console.log(cachedModelData);
 
-    const modelData =
-      typeof chatType === "string"
-        ? await DB1.collection("models").findOne({ title: chatType })
-        : chatType;
-    if (!modelData) throw new Error("AI Model not found");
+    if (!cachedModelData) {
+      const doc = await DB1.collection("models").findOne(
+        { "models.title": chatType },
+        { projection: { "models.$": 1 } }
+      );
+      const modelData = doc?.models?.[0];
+      // console.log(modelData);
 
-    // Fill the prompt
-    const finalPrompt = fillTemplate(modelData.promptTemplate, {
+      if (!modelData) throw new Error("AI Model not found");
+      await setCache("modelData", modelData, 1000);
+    }
+    // Build system prompt
+    const systemPrompt = fillTemplate(modelData.promptTemplate, {
       name: person.name,
       RelUserName: person.RelUserName,
       relation: person.relation,
       behavior: person.behavior,
       language: person.language,
       nickname: person.nickname,
-      message: newMessage,
-    });
+    })
+      .replace(/\s+/g, " ")
+      .trim();
+
+    const messages = [
+      { role: "system", content: systemPrompt },
+      ...historyMessages,
+      { role: "user", content: newMessage },
+    ];
 
     const completion = await openai.chat.completions.create({
       model: "gpt-4o-mini",
-      messages: [{ role: "user", content: finalPrompt }],
+      messages,
     });
 
     return completion.choices[0].message.content.trim();
